4147298c429818bae1cae8d70792a2402ac603e3
Unknown <martijn.millecamp@hotmail.com>
2018-11-09 13:14:21 +0100

    start using connexion

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask import request, jsonify
 from flask import make_response, render_template
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 import recommendations
+# import ratings
 import numpy as np
 
 app = Flask(__name__)
@@ -13,11 +15,11 @@ api = Api(app)
 class Home(Resource):
     def get(self):
         headers = {'Content-Type': 'text/html'}
-        return make_response(render_template('index.html'), 200, headers)
+        return make_response(render_template('home.html'), 200, headers)
 
 class Recommendation(Resource):
     def get(self):
-        return "Please give in your userId in the url "
+        return "Invalid request, no user id"
 
 class Recommendation_Name(Resource):
     def get(self, user_id):
@@ -31,10 +33,30 @@ class Recommendation_Name(Resource):
         score = rowSeries.values[1]
         return item
 
+class Ratings(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('user', type=int)
+        parser.add_argument('item', type=int)
+        request = parser.parse_args()
+        user_id = request['user']
+        item_id = request['item']
+        rating = ratings.getRating(user_id, item_id)
+        print(rating)
+        print(type(rating))
+        data = [
+            {'user_id': user_id,
+            'item_id': item_id,
+            'rating': rating
+            }
+        ]
+        return jsonify(data)
 
 api.add_resource(Home, '/home')
 api.add_resource(Recommendation, '/recommendation')
 api.add_resource(Recommendation_Name, '/recommendation/<user_id>')
+api.add_resource(Ratings, '/ratings')
+
 
 
 @app.route("/")
