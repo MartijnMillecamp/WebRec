6b33f7c817f848cc858656ed32ab1a09c67b78a1
Unknown <martijn.millecamp@hotmail.com>
2019-01-16 15:18:37 +0100

    implementing als script

--- recommendations_old.py
+++ recommendations_old.py
@@ -5,6 +5,8 @@ from lenskit import crossfold as xf
 from lenskit.algorithms import item_knn as knn
 from lenskit.algorithms import funksvd as funk
 from lenskit.algorithms import als
+from lenskit.algorithms import mf_common
+
 
 # read in the movielens 100k ratings with pandas
 # https://grouplens.org/datasets/movielens/100k/
@@ -19,6 +21,7 @@ algoFunk = funk.FunkSVD(2)
 algoAls = als.BiasedMF(6)
 
 
+
 # split the data in a test and a training set
 # for each user leave one row out for test purpose
 data = ratings
@@ -28,59 +31,50 @@ for (trainSet, testSet) in splits:
     train = trainSet
     test = testSet
 
-# train model
+# train models
 modelKNN = algoKNN.fit(train)
 modelFunk = algoFunk.fit(train)
 
 fittableALS = util.clone(algoAls)
 modelAls = fittableALS.fit(train)
-# select a user
+print(modelAls.user_features_)
+print(modelAls.item_features_)
+
 users = test.user.unique()
 
-def getRecommendationsALS(user):
-    model = modelAls
-    algo = algoAls
-    nb_recommendations = 1
-    recs = batch.recommend(algo, users, 100,
+def getRecommendations(user, model):
+    recs = batch.recommend(model, users, 100,
                            topn.UnratedCandidates(train), test)
-    recs = batch.recommend(algo, model, users,
-                           nb_recommendations, topn.UnratedCandidates(train))
     return recs[recs['user'] == user]
 
 
-def getRecommendations(user, algorithm):
+def getRecommendationsController(user, algorithm):
     '''
     Return a recommendation
     :param user: id of user you want a recommendation
     :param algorithm: "Funk" or "KNN"
     :return:
     '''
+    #default
     model = modelAls
-    algo = algoAls
 
     if algorithm == "Funk":
         print("Funk")
-        algo = algoFunk
         model = modelFunk
 
     elif algorithm == "KNN":
         print("KNN")
-        algo = algoKNN
         model = modelKNN
 
     elif algorithm == "ALS":
         model = modelAls
-        algo = algoAls
 
-    # Generate $nb_recommendations for the givenuser
-    nb_recommendations = 1
-    recs = batch.recommend(algo, model, users, nb_recommendations, topn.UnratedCandidates(train))
-    return recs[recs['user'] == user]
+    return getRecommendations(user, model)
 
 
 # Only for test purpose
 user = np.array(users[0])
-rec = getRecommendationsALS(user)
+rec = getRecommendationsController(user, "ALS")
 # select columns
 recColumns = rec[['item', 'score']]
 # select row (normally not needed)
@@ -89,4 +83,3 @@ item = rowSeries.values[0]
 score = rowSeries.values[1]
 print(item)
 print(score)
-print(type(item))
